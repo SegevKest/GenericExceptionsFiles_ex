73fc5464815f8e1b637e1321ce6967284d167393
Segev <segev.kest@gmail.com>
2022-05-06 21:23:25 +0300

    First init. created base class, fix to isMember + insert: implement equals + compareTo

---
+++ src/aSet.java
@@ -0,0 +1,169 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class aSet<T extends Comparable<T>> {
+
+	private ArrayList<T> setArray;
+
+	// Empty constructor
+	public aSet() {
+		setArray = new ArrayList<T>();
+	}
+
+	// Constructor with an array of the Generic type - insert each element to the
+	// set
+	public aSet(T[] paramArr) {
+
+		for (T arg : paramArr) {
+			insert(arg);
+		}
+	}
+
+	public boolean equals(Object setObj) {
+
+		aSet<T> toCompare;
+
+		if (setObj == null || !(setObj instanceof aSet))
+			return false;
+
+		
+		if (set.compareTo(set1) == 0)
+			return true;
+
+		return false;
+	}
+
+	// The method return true if the obj exist in the Set already, false if not.
+	public boolean isMember(T obj) {
+
+		if (setArray.contains(obj))
+			return true;
+		return false;
+
+	}
+
+	// The method will insert a Generic type variable to the set - will return true
+	// if inserted, false if not
+	public boolean insert(T objToInsert) {
+
+		if (this.isMember(objToInsert))
+			return false;
+		else
+			setArray.add(objToInsert);
+
+		return true;
+
+	}
+
+	// The method will delete the parameter Obj from the Set (if exist), if not
+	// exist - do nothing
+	public void delete(T objToRemove) {
+
+		int indexOfObj;
+
+		if (this.isMember(objToRemove)) {
+			indexOfObj = setArray.indexOf(objToRemove);
+			// setArray.remove(objToRemove);
+			setArray.remove(indexOfObj);
+		}
+	}
+
+	// The method will print the Set
+	public String toString() {
+
+		String returnedStr = "{";
+
+		for (int i = 0; i < setArray.size() - 1; i++)
+			returnedStr += setArray.get(i) + ",";
+
+		if (setArray.size() > 0)
+			returnedStr += setArray.get(setArray.size() - 1);
+
+		returnedStr += "}";
+
+		return returnedStr;
+	}
+
+	// the method will return the Iterator object for this Set
+	public Iterator<T> iterator() {
+
+		return setArray.iterator();
+	}
+
+	// The method get new set and calculate its union with the current set
+	public void union(aSet<T> newSetForUnion) {
+
+		Iterator<T> newSetIterator = newSetForUnion.iterator();
+
+		while (newSetIterator.hasNext()) {
+
+			T currMem = newSetIterator.next();
+
+			if (!this.isMember(currMem)) {
+				this.insert(currMem);
+			}
+		}
+	}
+
+	// The method gets a new set and return the intersection of the 2 groups -
+	// inside the caller
+	public void intersect(aSet<T> newSetForIntersect) {
+
+		ArrayList<T> theIntersect = new ArrayList<T>();
+
+		T currMem;
+
+		Iterator<T> currIterator = this.iterator();
+
+		while (currIterator.hasNext()) {
+
+			currMem = currIterator.next();
+
+			if (newSetForIntersect.isMember(currMem)) {
+				theIntersect.add(currMem);
+			}
+		}
+
+		setArray = new ArrayList<>(theIntersect);
+	}
+
+	// The method return true if the given set it a subset of the current set. false
+	// if not
+	public boolean isSubset(aSet<T> possibleSubSet) {
+
+		Iterator<T> setIterator = possibleSubSet.iterator();
+		T currMem;
+
+		while (setIterator.hasNext()) {
+			currMem = setIterator.next();
+
+			if (!this.isMember(currMem))
+				return false;
+		}
+		return true;
+	}
+
+	
+	  public static void main(String[] args) {
+	  
+	  
+	  aSet<String> set = new aSet<String>(); 
+	  aSet<String> set1 = new aSet<String>();
+	  
+	  
+	  System.out.println( set.equals(set1));
+	  
+	  set.insert("Segev"); set.insert("hey"); set.insert("gev");
+	  set.insert("Shona");
+	  
+	  set1.insert("gev"); set1.insert("gev");
+	  System.out.println( set.equals(set1));
+	  System.out.println(set); System.out.println(set1);
+	  
+	  
+	  set.intersect(set1);
+	  
+	  
+	  System.out.println(set); }
+	 
+}
---
+++ src/integerSet.java
@@ -0,0 +1,54 @@
+import java.util.Iterator;
+import java.util.Random;
+
+public class integerSet {
+
+	
+	
+	public static void fillSetWithRandomNumbers( aSet<Integer> set) {
+		
+		boolean insertResult;
+		final int UP_LIMIT = 100, NUMS = 10;
+		int i=0;
+		
+		Random rnd = new Random();
+		Iterator<Integer> iter = set.iterator();
+		
+		while(i<NUMS) {
+			
+			insertResult = set.insert(rnd.nextInt(UP_LIMIT));
+			
+			if (insertResult)
+				i++;
+		}
+	}
+	
+	public static aSet<Integer> createSetFromUser(){
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		aSet<Integer> set1 = new aSet<Integer>();
+		aSet<Integer> set2 = new aSet<Integer>();
+		aSet<Integer> set3 = new aSet<Integer>();
+		
+		fillSetWithRandomNumbers(set1);
+		fillSetWithRandomNumbers(set2);
+		fillSetWithRandomNumbers(set3);
+		
+		System.out.println("Sets");
+		System.out.println("Set 1 "+ set1);
+		System.out.println("Set 2 "+ set2);
+		System.out.println("Set 3 "+ set3);
+		
+		System.out.println("\nUnion of 2 first sets");
+		set1.union(set2);
+		System.out.println("Result "+ set1);
+		
+		System.out.println("\nIntersection of 2 remain sets");
+		set1.intersect(set3);
+		System.out.println("Result "+ set1);
+		
+	}
+}
